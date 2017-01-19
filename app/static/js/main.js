8e4405bb49b1151317118908638fd6fe7bf64187
Bishakh Ghosh <ghoshbishakh@gmail.com>
2017-01-19 18:26:15 +0530

    Add search functionality

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -48,3 +48,24 @@ $('.market-buy-button').click(function(){
     console.log(parseInt(pcash/stockprice));
     $('.maxqty').text(parseInt(pcash/stockprice));
 });
+
+function leaderboardSearch() {
+  // Declare variables
+  var input, filter, table, tr, td, i;
+  input = document.getElementById("leaderboard-search");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("leaderboard_table_body");
+  tr = table.getElementsByTagName("tr");
+
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[1];
+    if (td) {
+      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
